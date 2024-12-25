5657409c334b20e5d6e01e17b87a67db2ea5974b
Sandro Chkhaidze <116597866+Sandrochkhaidzee@users.noreply.github.com>
2024-12-25 21:34:35 +0400

    Update EventDetails.js

--- JS/EventDetails.js
+++ JS/EventDetails.js
@@ -5,11 +5,17 @@ function getEventDetails() {
     const eventDescription = JSON.parse(urlParams.get('eventDescription'));
     const location = urlParams.get('location');
     const picture = urlParams.get('picture');
+    
+    const eventPrice = urlParams.get('eventPrice');
+    const eventStartTime = urlParams.get('eventStartTime');
+    const quizAmount = urlParams.get('quizAmount');
+    const topicsAmount = urlParams.get('topicsAmount');
+    const seatsAmount = urlParams.get('seatsAmount');
 
     document.getElementById('event-name').textContent = eventName;
-    document.getElementById('event-date').textContent = `${formatEventDate(eventDate)}`;
     document.getElementById('event-location').textContent = `${location}`;
     document.getElementById('event-image').src = picture;
+    
     const descriptionContainer = document.getElementById('event-description');
     descriptionContainer.innerHTML = '';
 
@@ -22,6 +28,15 @@ function getEventDetails() {
         sectionText.textContent = section.text;
         descriptionContainer.appendChild(sectionText);
     });
+
+    document.getElementById('event-price').textContent = eventPrice;
+    document.getElementById('event-start-time').textContent = eventStartTime;
+    document.getElementById('quiz-amount').textContent = quizAmount;
+    document.getElementById('event-date').textContent = `${formatEventDate(eventDate)}`;
+    document.getElementById('event-topics').textContent = topicsAmount;
+    document.getElementById('total-seat-amount').textContent = seatsAmount;
+
+    document.querySelector('.floating-event-details').classList.add('show');
 }
 
 function formatEventDate(date) {
